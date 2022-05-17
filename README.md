ee9a272335d73b58ff02358793ba0b0b5794fba3
Markus Egon Kuhn <markus.egon.kuhn@gmail.com>
2022-05-17 15:56:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+simple project to get asynchronous tasks in command line running more asthetic for nodejs
+
+````
+const executeCommand = require('command-execution-await')
+await executeCommand("ls")
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const { exec } = require('child_process');
+
+const executeCommand = (command) => {
+	return new Promise((resolve, reject) => {
+		const process = exec(command)
+		process.stdout.on('data', (data) => {
+			console.log('stdout: ' + data.toString())
+		})
+
+		process.stderr.on('data', (data) => {
+			console.log('stderr: ' + data.toString())
+		})
+
+		process.on('exit', (code) => {
+			console.log('child process exited with code ' + code.toString())
+			resolve(true)
+		})
+	})
+
+}
+module.exports = executeCommand;
